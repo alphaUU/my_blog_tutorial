17c2ae7a1862a8c1175062988464fe61e8f784d1
yyy <971610324@qq.com>
2016-08-25 17:02:16 +0800

    add some new functions

--- article/views.py
+++ article/views.py
@@ -1,5 +1,5 @@
 # Create your views here.
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from article.models import Article
 from datetime import datetime
@@ -16,3 +16,36 @@ def detail(request, id):
 def home(request):
     post_list = Article.objects.all()
     return render(request, 'home.html', {'post_list':post_list})
+
+def archives(request):
+    try:
+        post_list = Article.objects.all()
+    except Article.DoesNotExist:
+        raise Http404
+    return render(request, 'archives.html',{'post_list':post_list,
+                                            'error':False})
+
+def about_me(request):
+    return render(request, 'aboutme.html')
+
+def search_tag(request,tag):
+    try:
+        post_list = Article.objects.filter(category__iexact = tag)
+    except Article.DoesNotExist:
+        raise Http404
+    return render(request, 'tag.html', {'post_list':post_list})
+
+def blog_search(request):
+    if 's' in request.GET:
+        s = request.GET['s']
+        if not s:
+            return render(request,'home.html')
+        else:
+            post_list = Article.objects.filter(title__icontains = s)
+            if len(post_list) == 0:
+                return render(request,'archives.html',{'post_list':post_list,
+                                                       'error':True})
+            else:
+                return render(request,'archives.html',{'post_list':post_list,
+                                                       'error':False})
+    return redirect('/')
